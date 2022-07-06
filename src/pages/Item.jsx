968fb9469e6fb2df792548826560aec006793bc7
L2HYUNN <dhl9810@naver.com>
2022-07-06 19:53:19 +0900

    Amend Item component

--- src/pages/Item.jsx
+++ src/pages/Item.jsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 // import Mens New Arrivlas images
-// import mensNewArrivals from "../../assets/mens/new-arrivals/mens_outer1.webp";
+import mensNewArrivals from "../assets/mens/new-arrivals/mens_outer1.webp";
 // import mensNewArrivals2 from "../../assets/mens/new-arrivals/mens_bottom1.webp";
 // import mensNewArrivals3 from "../../assets/mens/new-arrivals/mens_outer2.webp";
 // import mensNewArrivals4 from "../../assets/mens/new-arrivals/mens_top1.webp";
@@ -20,40 +20,64 @@ import { useLocation } from "react-router-dom";
 import { fetchItemInfo } from "../api";
 
 const Container = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 35.5rem);
-  grid-template-rows: repeat(4, 59.7rem);
-  row-gap: 2.7rem;
-  column-gap: 1.3rem;
+  display: flex;
   justify-content: center;
   min-width: 144rem;
   margin-top: 4.2rem;
   margin-bottom: 22rem;
 `;
-const Contents = styled.div``;
-
-const Img = styled.img`
-  width: 35.5rem;
-  height: 52.8rem;
+const ItemContents = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 72rem;
+  height: 82rem;
+  padding: 6rem 14rem;
+`;
+const ItemImg = styled.img`
+  width: 52rem;
+  height: 82rem;
+  margin: 0 10rem;
   margin-bottom: 1.8rem;
   cursor: pointer;
 `;
-const Texts = styled.div`
+const ItemTexts = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   font-size: 1.2rem;
-  font-weight: 500;
+`;
+const ItemTitle = styled.span`
+  font-size: 1.8rem;
+  font-weight: 600;
   text-transform: uppercase;
+  margin-bottom: 2rem;
 `;
-const Text = styled.span`
+const ItemBar = styled.div`
+  width: 4rem;
+  height: 1rem;
+  border-bottom: 1px solid black;
+  margin-bottom: 6rem;
+`;
+const ItemText = styled.span`
+  width: 33rem;
   margin-bottom: 1.2rem;
+  text-align: center;
+  margin-bottom: 6rem;
   cursor: pointer;
-  &:last-child {
-    font-weight: 400;
-  }
 `;
+const ItemButton = styled.div`
+  background-color: transparent;
+  border: none;
+  border-bottom: 1px solid black;
+  padding-bottom: 0.5rem;
+  font-size: 1.3rem;
+  font-weight: 600;
+  cursor: pointer;
+`;
+const ItemList = styled.ul``;
+const ItemListText = styled.li``;
 
 function Item() {
   const {
@@ -64,7 +88,21 @@ function Item() {
   return (
     <>
       <Header />
-      <main></main>
+      <main>
+        <Container>
+          <ItemImg src={mensNewArrivals} />
+          <ItemContents>
+            <ItemTexts>
+              <ItemTitle>{data.itemName}</ItemTitle>
+              <ItemBar></ItemBar>
+              <ItemText>{data.itemDescription}</ItemText>
+            </ItemTexts>
+            <ItemButton>Select Option</ItemButton>
+            <ItemList></ItemList>
+            <ItemButton>Add to Cart</ItemButton>
+          </ItemContents>
+        </Container>
+      </main>
       <Footer />
     </>
   );
