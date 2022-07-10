2fd5868b137a6cf5fd4d19bfda78e841a33158de
L2HYUNN <dhl9810@naver.com>
2022-07-10 23:47:38 +0900

    Amend Item Component

--- src/pages/Item.jsx
+++ src/pages/Item.jsx
@@ -18,6 +18,7 @@ import mensNewArrivals from "../assets/mens/new-arrivals/mens_outer1.webp";
 // import mensNewArrivals12 from "../../assets/mens/new-arrivals/mens_outer5.webp";
 import { useLocation } from "react-router-dom";
 import { fetchItemInfo } from "../api";
+import { useState } from "react";
 
 const Container = styled.div`
   display: flex;
@@ -62,10 +63,18 @@ const ItemBar = styled.div`
 `;
 const ItemText = styled.span`
   width: 33rem;
+  height: 29rem;
   margin-bottom: 1.2rem;
   text-align: center;
   margin-bottom: 6rem;
-  cursor: pointer;
+`;
+const ItemPrice = styled.span`
+  width: 33rem;
+  margin-bottom: 1.2rem;
+  text-align: center;
+  margin-bottom: 6rem;
+  font-size: 1.3rem;
+  font-weight: 600;
 `;
 const ItemButton = styled.div`
   background-color: transparent;
@@ -75,15 +84,56 @@ const ItemButton = styled.div`
   font-size: 1.3rem;
   font-weight: 600;
   cursor: pointer;
+  width: 33rem;
+  text-align: center;
+`;
+const ItemSelect = styled.select`
+  background-color: transparent;
+  border: none;
+  border-bottom: 1px solid black;
+  padding-bottom: 0.5rem;
+  font-size: 1.3rem;
+  font-weight: 600;
+  width: 33rem;
+  margin-bottom: 5rem;
+`;
+const ItemOption = styled.option``;
+const ItemBox = styled.div`
+  width: 33rem;
+  height: 13.5rem;
+  padding: 5rem;
 `;
 const ItemList = styled.ul``;
-const ItemListText = styled.li``;
+const ItemListText = styled.li`
+  font-size: 1.3rem;
+  font-weight: 600;
+`;
+const ItemOptionText = styled.span``;
+const ItemOptionPrice = styled.span``;
+const ItemReview = styled.div`
+  width: 52rem;
+  height: 20rem;
+  border: 1px solid black;
+`;
+const ItemRelated = styled.div`
+  width: 58rem;
+`;
 
 function Item() {
+  const [item, setItem] = useState([]);
   const {
     state: { id },
   } = useLocation();
-  const { data } = useQuery(["ItemInfo", id], () => fetchItemInfo(id));
+  const { isLoading, data } = useQuery(["ItemInfo", id], () =>
+    fetchItemInfo(133)
+  );
+  // console.log(data.images);
+
+  const onSelect = (event) => {
+    console.log(event.target.value);
+    console.dir(event.target);
+    console.log("Hello");
+  };
 
   return (
     <>
@@ -93,12 +143,33 @@ function Item() {
           <ItemImg src={mensNewArrivals} />
           <ItemContents>
             <ItemTexts>
-              <ItemTitle>{data.itemName}</ItemTitle>
+              <ItemTitle>{isLoading ? null : data.itemName}</ItemTitle>
               <ItemBar></ItemBar>
-              <ItemText>{data.itemDescription}</ItemText>
+              <ItemPrice>{isLoading ? null : data.itemPrice} KRW</ItemPrice>
+              <ItemText>{isLoading ? null : data.itemDescription}</ItemText>
             </ItemTexts>
-            <ItemButton>Select Option</ItemButton>
-            <ItemList></ItemList>
+            <ItemSelect onChange={onSelect}>
+              <ItemOption value="">Select Option</ItemOption>
+              {isLoading
+                ? null
+                : data.options.map((option) => {
+                    return (
+                      <ItemOption
+                        key={option.optionId}
+                        value={option.optionId}
+                        name={option.optionContent}
+                      >
+                        {option.optionContent}
+                      </ItemOption>
+                    );
+                  })}
+            </ItemSelect>
+            <ItemList>
+              <ItemListText>
+                <ItemOptionText></ItemOptionText>
+                <ItemOptionPrice></ItemOptionPrice>
+              </ItemListText>
+            </ItemList>
             <ItemButton>Add to Cart</ItemButton>
           </ItemContents>
         </Container>
