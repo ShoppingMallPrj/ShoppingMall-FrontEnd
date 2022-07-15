38b5b5341c02500d56e69133f272ef3395c2e45d
L2HYUNN <dhl9810@naver.com>
2022-07-15 13:11:39 +0900

    Amend Inquiry

--- src/pages/inquiry/Inquiry.jsx
+++ src/pages/inquiry/Inquiry.jsx
@@ -7,11 +7,58 @@ import Pagenation from "../../components/Pagenation";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100rem;
+  height: 92rem;
+  min-width: 100rem;
+  margin-bottom: 22rem;
+`;
+const InquiryTitle = styled.div`
+  font-size: 1.7rem;
+  font-weight: 600;
+  margin-bottom: 6.5rem;
+`;
+const InquiryList = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100rem;
+  height: 92rem;
+  padding: 5rem;
+  min-width: 100rem;
+  border: 1px solid black;
+`;
+const InquiryText = styled.div`
+  display: flex;
+  width: 80rem;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 1.6rem;
+  font-weight: 400;
+  margin-bottom: 2rem;
+  padding-bottom: 1rem;
+  border-bottom: 1px solid black;
+`;
+const InquiryName = styled.span`
+  width: 80%;
+`;
+const InquiryEmail = styled.span`
+  width: 20%;
+`;
+const InquiryWrite = styled.div`
+  width: 78rem;
+  text-align: right;
+  font-size: 1.4rem;
+  margin-bottom: 1rem;
+`;
+
 //한번에 불러올 페이지 사이즈
 const pageSize = 10;
 
 function Inquiry() {
-
   const [inquiryState, setInquiryState] = useState({
     content: [],
     totalPages: 0, // 전체 페이지
@@ -49,10 +96,14 @@ function Inquiry() {
         {inquiryState.content.map((inquiry) => {
           return (
             <>
-              <Link to={`/inquiry/${inquiry.inquiryId}`}>
-                {inquiry.inquiryTitle}
-              </Link>
-              <h1>{inquiry.userEmail}</h1>
+              <InquiryText>
+                <Link to={`/inquiry/${inquiry.inquiryId}`}>
+                  <InquiryName>{inquiry.inquiryTitle}</InquiryName>
+                </Link>
+                <InquiryEmail>
+                  <h1>{inquiry.userEmail}</h1>
+                </InquiryEmail>
+              </InquiryText>
             </>
           );
         })}
@@ -64,18 +115,21 @@ function Inquiry() {
     <>
       <Header />
       <main>
-        <Body></Body>
-        {/* 글 작성 링크 */}
-        <Link to={`/inquiry/create`}>문의 작성</Link>
-        {/* 페이지네이션 */}
-        <Pagenation
-          total={inquiryState.totalPages}
-          onChange={async (page) => fetch(page - 1)}
-          //fetch={(page) => fetch(page)}
-          hideNav={true}
-        ></Pagenation>
+        <Container>
+          <InquiryTitle>Q & A</InquiryTitle>
+          <InquiryList>
+            <Body></Body>
+            <InquiryWrite>
+              <Link to={`/inquiry/create`}>문의 작성</Link>
+            </InquiryWrite>
+            <Pagenation
+              total={inquiryState.totalPages}
+              onChange={async (page) => fetch(page - 1)}
+              hideNav={true}
+            ></Pagenation>
+          </InquiryList>
+        </Container>
       </main>
-
       <Footer />
     </>
   );
--- src/pages/inquiry/InquiryCreate.jsx
+++ src/pages/inquiry/InquiryCreate.jsx
@@ -1,11 +1,10 @@
 import { createInquiry } from "../../api";
 import { useForm } from "react-hook-form";
-
+import styled from "styled-components";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 
 function InquiryCreate() {
-
   const {
     register,
     handleSubmit,
@@ -18,23 +17,62 @@ function InquiryCreate() {
     try {
       const token = JSON.parse(sessionStorage.getItem("user")).token;
       const res = await createInquiry(token, data);
-      console.log(res)
+      console.log(res);
     } catch (error) {
       console.log(error);
     }
-  };  
+  };
+
+  const InquiryForm = styled.div`
+    display: flex;
+    flex-direction: column;
+    /* align-items: center; */
+    width: 100rem;
+    height: 92rem;
+    min-width: 100rem;
+    /* border: 1px solid black; */
+    font-size: 1.4rem;
+    margin-bottom: 22rem;
+  `;
+  const InquiryTitle = styled.span`
+    font-size: 1.8rem;
+    padding: 1rem;
+  `;
+  const InquiryContent = styled.span`
+    font-size: 1.8rem;
+    padding: 1rem;
+  `;
+  const InquiryInput = styled.input`
+    font-size: 1.4rem;
+  `;
+  const InquiryTextarea = styled.textarea`
+    font-size: 1.4rem;
+    height: 20rem;
+    margin-bottom: 5rem;
+  `;
+  const InquiryButton = styled.button`
+    background-color: transparent;
+    border: none;
+    border-bottom: 1px solid black;
+    padding-bottom: 0.5rem;
+    font-size: 1.6rem;
+    cursor: pointer;
+    &:last-child() {
+      font-weight: 600;
+    }
+  `;
 
   return (
     <>
       <Header />
       <main>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div>제목</div>
-          <input {...register("inquiryTitle")} />
-          <div>내용</div>
-          <input {...register("inquiryContent")} />
-          <button type="submit">전송</button>
-        </form>
+        <InquiryForm onSubmit={handleSubmit(onSubmit)}>
+          <InquiryTitle>제목</InquiryTitle>
+          <InquiryInput {...register("inquiryTitle")} />
+          <InquiryContent>내용</InquiryContent>
+          <InquiryTextarea {...register("inquiryContent")} />
+          <InquiryButton type="submit">Write</InquiryButton>
+        </InquiryForm>
       </main>
       <Footer />
     </>
